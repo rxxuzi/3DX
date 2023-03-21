ab87e051c30b6207230519ebc7122f1625bf368b
Fukuda <75666622+rxxuzi@users.noreply.github.com>
2023-03-21 21:08:15 +0900

    Ver 2.1

---
+++ src/main2_1e/Main.java
@@ -0,0 +1,29 @@
+package main2_1e;
+
+import javax.swing.JFrame;
+import java.awt.*;
+/**
+ * @author 1futureX
+ * @version 1.20
+ */
+@SuppressWarnings("serial")
+public class Main extends JFrame{
+	static Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	static long StartUpTime = System.currentTimeMillis();
+	Screen Panel = new Screen();
+	public Main() {
+		setUndecorated(false);//trueにするとバーが消える
+		setSize(ScreenSize);//全画面表示
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocation(0,0);
+		setTitle("3D Graphics Ver 2.1e ");
+		add(Panel); // Screen.javaの内容をFrameに追加する。
+	}
+	public static void main(String[] args) {
+		JFrame f = new Main();
+		f.setVisible(true);
+		f.getJMenuBar();
+	}
+
+}
